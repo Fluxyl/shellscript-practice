f8ebc2d53ed1639e7add7aa2008fd78d4e33c5c9
Fluxyl <91804289+Fluxyl@users.noreply.github.com>
2021-10-27 14:20:42 -0500

    removed redundant file

--- gitSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This is a script I created to automate setting up git.
-
-# Check if git is installed
-if ! command -v git &> /dev/null
-then
-    echo "<the_command> could not be found"
-    exit
-fi
