a1dd3dcbe9a978bdb40cec5110efa8fefaa8b4ab
Fluxyl <91804289+Fluxyl@users.noreply.github.com>
2021-10-27 14:12:19 -0500

    Added finishing touches to first5.sh

--- first5.sh
+++ first5.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/sh
 
 # Use the dummy files from first4
+NUM=0
+
+# For Loop with iterator, done entirely in shell, no bash.
+#       It was the hardest thing to figure out incrementing.
 for FILENAME in *_dummy; do
     [ -f "$FILENAME" ] || continue
-    echo "$FILENAME"
+    echo "${NUM}: ${FILENAME}"
+    NUM=$(expr $NUM + 1)
 done
